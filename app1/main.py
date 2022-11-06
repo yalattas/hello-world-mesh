e90e21d74403cc6e0324455dac3030a38c1ea1e1
Yaser Alattas <y.alattas@gmail.com>
2022-11-06 10:47:08 +0300

    sample app

---
+++ app1/main.py
@@ -0,0 +1,28 @@
+from typing import Union
+from fastapi import FastAPI
+import requests
+import json
+import os, uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    try:
+        response = requests.get('http://app2:8002/sample').text
+        response = json.loads(response)
+    except KeyError as e:
+        print('error in network layer: ', e)
+    return {
+        "first function":"here I am",
+        "Hello": response['function']
+    }
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=int(os.environ.get('APP_PORT', default=8000)), reload=True)
---
+++ app2/main.py
@@ -0,0 +1,18 @@
+from typing import Union
+from fastapi import FastAPI
+import os, uvicorn
+
+app = FastAPI()
+
+
+@app.get("/sample")
+def read_root():
+    return {"function": "hello from function 2"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=int(os.environ.get('APP_PORT', default=8000)), reload=True)
